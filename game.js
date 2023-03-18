e1c4f4dec3e50275230f6c557d19887ec057d9df
tekelsey <tekelsey@gmail.com>
2023-03-18 13:20:47 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,80 +1,23 @@
 const mazeElement = document.getElementById('maze');
 
+// Add button references
+const btnUp = document.getElementById('btnUp');
+const btnDown = document.getElementById('btnDown');
+const btnLeft = document.getElementById('btnLeft');
+const btnRight = document.getElementById('btnRight');
+
 const gerbil = 'G';
 const seed = 'S';
 const wall = '#';
 const empty = ' ';
 
-const maze = [
-  [wall, wall, wall, wall, wall, wall, wall, wall, wall, wall],
-  [wall, empty, empty, empty, empty, empty, empty, empty, empty, wall],
-  [wall, empty, wall, wall, wall, wall, empty, wall, seed, wall],
-  [wall, gerbil, wall, empty, empty, empty, empty, wall, empty, wall],
-  [wall, empty, wall, wall, wall, wall, empty, wall, empty, wall],
-  [wall, empty, empty, empty, empty, empty, empty, wall, empty, wall],
-  [wall, empty, wall, wall, wall, wall, empty, wall, empty, wall],
-  [wall, seed, wall, empty, empty, empty, empty, wall, empty, wall],
-  [wall, empty, empty, empty, empty, empty, empty, empty, empty, wall],
-  [wall, wall, wall, wall, wall, wall, wall, wall, wall, wall],
-];
+// ... rest of the code ...
 
-function renderMaze() {
-  let mazeString = '';
-
-  for (let y = 0; y < maze.length; y++) {
-    for (let x = 0; x < maze[y].length; x++) {
-      mazeString += maze[y][x];
-    }
-    mazeString += '\n';
-  }
-
-  mazeElement.textContent = mazeString;
-}
-
-function findGerbil() {
-  for (let y = 0; y < maze.length; y++) {
-    for (let x = 0; x < maze[y].length; x++) {
-      if (maze[y][x] === gerbil) {
-        return { x, y };
-      }
-    }
-  }
-}
-
-function moveGerbil(dx, dy) {
-  const { x, y } = findGerbil();
-  const newX = x + dx;
-  const newY = y + dy;
-
-  if (maze[newY][newX] === wall) {
-    return;
-  }
-
-  if (maze[newY][newX] === seed) {
-    console.log('Gerbil ate a sunflower seed!');
-  }
-
-  maze[y][x] = empty;
-  maze[newY][newX] = gerbil;
-  renderMaze();
-}
-
-function handleKeydown(event) {
-  switch (event.key) {
-    case 'ArrowUp':
-      moveGerbil(0, -1);
-      break;
-    case 'ArrowDown':
-      moveGerbil(0, 1);
-      break;
-    case 'ArrowLeft':
-      moveGerbil(-1, 0);
-      break;
-    case 'ArrowRight':
-      moveGerbil(1, 0);
-      break;
-  }
-}
+// Add event listeners for button clicks
+btnUp.addEventListener('click', () => moveGerbil(0, -1));
+btnDown.addEventListener('click', () => moveGerbil(0, 1));
+btnLeft.addEventListener('click', () => moveGerbil(-1, 0));
+btnRight.addEventListener('click', () => moveGerbil(1, 0));
 
 renderMaze();
 document.addEventListener('keydown', handleKeydown);
