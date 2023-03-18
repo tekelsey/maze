234f074ad6253145a8780b1bab6ea18148015801
tekelsey <tekelsey@gmail.com>
2023-03-18 13:26:36 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,11 +1,3 @@
-const mazeElement = document.getElementById('maze');
-
-// Add button references
-const btnUp = document.getElementById('btnUp');
-const btnDown = document.getElementById('btnDown');
-const btnLeft = document.getElementById('btnLeft');
-const btnRight = document.getElementById('btnRight');
-
 const gerbil = 'G';
 const seed = 'S';
 const wall = '#';
@@ -13,11 +5,17 @@ const empty = ' ';
 
 // ... rest of the code ...
 
-// Add event listeners for button clicks
-btnUp.addEventListener('click', () => moveGerbil(0, -1));
-btnDown.addEventListener('click', () => moveGerbil(0, 1));
-btnLeft.addEventListener('click', () => moveGerbil(-1, 0));
-btnRight.addEventListener('click', () => moveGerbil(1, 0));
+window.onload = function () {
+  const btnUp = document.getElementById('btnUp');
+  const btnDown = document.getElementById('btnDown');
+  const btnLeft = document.getElementById('btnLeft');
+  const btnRight = document.getElementById('btnRight');
 
-renderMaze();
-document.addEventListener('keydown', handleKeydown);
+  btnUp.addEventListener('click', () => moveGerbil(0, -1));
+  btnDown.addEventListener('click', () => moveGerbil(0, 1));
+  btnLeft.addEventListener('click', () => moveGerbil(-1, 0));
+  btnRight.addEventListener('click', () => moveGerbil(1, 0));
+
+  renderMaze();
+  document.addEventListener('keydown', handleKeydown);
+};
