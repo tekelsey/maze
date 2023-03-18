47afac0398db6146bbdede812dc7876a729d462a
tekelsey <tekelsey@gmail.com>
2023-03-18 15:02:06 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,9 +1,4 @@
-const mazeElement = document.getElementById('maze');
 
-const gerbil = '\uD83D\uDC2D';
-const seed = '\uD83C\uDF3B';
-const wall = '#';
-const empty = ' ';
 
 const maze = [
   ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
